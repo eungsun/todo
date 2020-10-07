522f5233496b9ccff804331051c81fe1299fd004
eungsun <eungsun@gmail.com>
2020-10-06 20:59:43 -0400

    add tasks to list

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("any string");
+const button = document.querySelector("#submit-todo-btn");
+const input = document.querySelector("#todo-field");
+const ul = document.querySelector("ul");
+
+function addTask() {
+  const item = document.createElement("li");
+  item.innerHTML = input.value;
+  ul.appendChild(item);
+}
+
+button.addEventListener("click", addTask);
